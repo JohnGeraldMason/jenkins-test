ceb6944dc8de806b6c4f92be0205c4f67642e970
John Mason <john.mason@qracorp.com>
2014-12-20 22:33:11 -0400

    testing

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-  it('jenkins testing 1 2 3', function(done) {
+  it('jenkins testing 1 2', function(done) {
     request(app).get('/').expect('jenkins testing 1 2 3', done);
   });
 });
